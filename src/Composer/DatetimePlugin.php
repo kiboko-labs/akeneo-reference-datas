6501b764ef409ddc8231ce189eb33e2d478e8a19
Grégory Planchat <gregory@luni.fr>
2016-11-14 12:59:17 +0100

    Testing plugin

--- src/Composer/DatetimePlugin.php
+++ src/Composer/DatetimePlugin.php
@@ -8,6 +8,14 @@ use Composer\Installer\PackageEvent;
 use Composer\IO\IOInterface;
 use Composer\Plugin\PluginEvents;
 use Composer\Plugin\PluginInterface;
+use Kiboko\Component\AkeneoProductValues\AnnotationGenerator\DoctrineColumnAnnotationGenerator;
+use Kiboko\Component\AkeneoProductValues\Builder\BundleBuilder;
+use Kiboko\Component\AkeneoProductValues\CodeGenerator\DoctrineEntity\DoctrineEntityScalarFieldCodeGenerator;
+use Kiboko\Component\AkeneoProductValues\CodeGenerator\DoctrineEntity\DoctrineEntityScalarFieldGetMethodCodeGenerator;
+use Kiboko\Component\AkeneoProductValues\CodeGenerator\DoctrineEntity\DoctrineEntityScalarFieldSetMethodCodeGenerator;
+use Kiboko\Component\AkeneoProductValues\CodeGenerator\ProductValueCodeGenerator;
+use League\Flysystem\Adapter\Local;
+use League\Flysystem\Filesystem;
 
 class DatetimePlugin implements PluginInterface, EventSubscriberInterface
 {
@@ -21,10 +29,18 @@ class DatetimePlugin implements PluginInterface, EventSubscriberInterface
      */
     protected $io;
 
+    /**
+     * @var Filesystem
+     */
+    private $filesystem;
+
     public function activate(Composer $composer, IOInterface $io)
     {
         $this->composer = $composer;
         $this->io = $io;
+        $this->filesystem = new Filesystem(
+            new Local(getcwd())
+        );
     }
 
     public static function getSubscribedEvents()
@@ -38,6 +54,38 @@ class DatetimePlugin implements PluginInterface, EventSubscriberInterface
 
     public function onPostPackageInstall(PackageEvent $event)
     {
-        var_dump($this->composer->getConfig()->all());
+        $root = $this->composer->getConfig()->get('akeneo-appbundle-root-dir') ?: 'src';
+        $vendor = $this->composer->getConfig()->get('akeneo-appbundle-vendor-name');
+        $bundle = $this->composer->getConfig()->get('akeneo-appbundle-bundle-name') ?: 'AppBundle';
+
+        if ($vendor === '') {
+            $namespace = $bundle;
+            $path = $bundle . '/';
+        } else {
+            $namespace = $vendor . '\\Bundle\\' . $bundle;
+            $path = $vendor . '/Bundle/' . $bundle . '/';
+        }
+
+        $productValueClass = new ProductValueCodeGenerator('ProductValue', $namespace . '\\Model');
+
+        $productValueClass->addMethod(
+            (new DoctrineEntityScalarFieldGetMethodCodeGenerator('created', 'DateTimeInterface'))
+        );
+
+        $productValueClass->addMethod(
+            (new DoctrineEntityScalarFieldSetMethodCodeGenerator('created', 'DateTimeInterface'))
+        );
+
+        $productValueClass->addUseStatement('DateTimeInterface');
+
+        $builder = new BundleBuilder();
+        $builder->setFileDefinition('Model/ProductValue.php',
+            [
+                $productValueClass->getNode()
+            ]
+        );
+
+        $builder->initialize($this->filesystem, $root . '/' . $path);
+        $builder->generate($this->filesystem, $root . '/' . $path);
     }
 }

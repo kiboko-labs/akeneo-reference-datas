37b9dc395a3557b562dd541dc290d97bad1f27b2
Grégory Planchat <gregory@luni.fr>
2016-11-14 12:33:33 +0100

    Initial commit

---
+++ src/Composer/DatetimePlugin.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Kiboko\AkeneoProductValuesPackage\Datetime\Composer;
+
+use Composer\Composer;
+use Composer\EventDispatcher\EventSubscriberInterface;
+use Composer\Installer\PackageEvent;
+use Composer\IO\IOInterface;
+use Composer\Plugin\PluginEvents;
+use Composer\Plugin\PluginInterface;
+
+class DatetimePlugin implements PluginInterface, EventSubscriberInterface
+{
+    /**
+     * @var Composer
+     */
+    protected $composer;
+
+    /**
+     * @var IOInterface
+     */
+    protected $io;
+
+    public function activate(Composer $composer, IOInterface $io)
+    {
+        $this->composer = $composer;
+        $this->io = $io;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            'post-package-install' => [
+                ['onPostAutoloadDump', 0]
+            ],
+        ];
+    }
+
+    public function onPostPackageInstall(PackageEvent $event)
+    {
+        var_dump($this->composer->getConfig()->all());
+        var_dump($event);
+    }
+}

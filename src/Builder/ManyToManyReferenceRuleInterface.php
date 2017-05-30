43c2dbd19118f88ca4b7fbf0574b5c756a4387a9
Grégory Planchat <gregory@kiboko.fr>
2017-05-31 00:25:12 +0200

    Fixed issue with interface reference

--- src/Builder/ManyToManyReferenceRuleInterface.php
+++ src/Builder/ManyToManyReferenceRuleInterface.php
@@ -2,6 +2,8 @@
 
 namespace Kiboko\Component\AkeneoProductValuesPackage\Builder;
 
+use Kiboko\Component\AkeneoProductValues\Builder\RuleInterface;
+
 interface ManyToManyReferenceRuleInterface extends RuleInterface
 {
     const TYPE = 'manyToMany';
--- src/Builder/ManyToOneReferenceRuleInterface.php
+++ src/Builder/ManyToOneReferenceRuleInterface.php
@@ -2,6 +2,8 @@
 
 namespace Kiboko\Component\AkeneoProductValuesPackage\Builder;
 
+use Kiboko\Component\AkeneoProductValues\Builder\RuleInterface;
+
 interface ManyToOneReferenceRuleInterface extends RuleInterface
 {
     const TYPE = 'manyToOne';
